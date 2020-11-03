ec063c0d88a74f9d587606842628caf21b262b2b
minhtien020900 <50957818+cobedoimatbuon0908@users.noreply.github.com>
2020-11-03 22:50:25 +0700

    Minh Tien create Dialog for demo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BootstrapVue,BootstrapVueIcons  } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+
+Vue.use(BootstrapVue,BootstrapVueIcons)
 Vue.use(VueRouter)
 import {routes} from './router'
 const router = new VueRouter({
